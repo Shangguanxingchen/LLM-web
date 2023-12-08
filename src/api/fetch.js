7f5208100fa5fb109244e4411bfed394913941ea
zhenghao <919811679@qq.com>
2023-12-08 14:37:39 +0800

    增肌token过期，重新登录

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
+import router from '@/router'
 
+const enable = true
 // 创建axios实例
 const service = axios.create({
 //   baseURL: process.env.NODE_ENV === "development" ? "/" : "https://xj.chinaamc.com:8000/" ,
@@ -26,6 +29,17 @@ service.interceptors.request.use(
 
 service.interceptors.response.use(
   (res) => {
+    if (res.data.code === 201 && enable) {
+      Message.error(`登录态失效，请重新登录！`)
+      localStorage.removeItem(`token`)
+      // enable = false
+      setTimeout(() => {
+        router.replace(`/login`)
+      }, 1000)
+      // setTimeout(() => {
+      //   enable = true
+      // }, 6000)
+    }
     return res.data ? res.data : Promise.reject(res)
   },
   (error) => Promise.reject(error)
